6b82a9589e246abc488c02f61ed5a868f267c0d1
GregPrzybyla <grzegorz.przybyla@untitledkingdom.co>
2016-12-10 17:29:06 +0100

    Package.swift update

--- Package.swift
+++ Package.swift
@@ -2,9 +2,11 @@ import PackageDescription
 
 let package = Package(
     name: "GPDatabaseDataHandler",
+    targets: [],
     dependencies: [
         .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-PostgreSQL.git", majorVersion: 2, minor: 0),
         .Package(url: "https://github.com/Hearst-DD/ObjectMapper.git", majorVersion: 2, minor: 2)
-    ]
+    ],
+    exclude: []
 )
